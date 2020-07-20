08b32e18b79219e117381b7fbe8090986207feea
Thomas Skerbis <thomas.skerbis@mac.com>
2020-07-20 15:26:14 +0200

    uk-fieldset

--- ytemplates/uikit3/value.fieldset.tpl.php
+++ ytemplates/uikit3/value.fieldset.tpl.php
@@ -1,5 +1,5 @@
 <?php if ($option == 'open'): ?>
-    <fieldset class="<?php echo $this->getHTMLClass(), ' ', $this->getElement(3) ?>" id="<?php echo $this->getHTMLId() ?>">
+    <fieldset class="uk-fieldset <?php echo $this->getHTMLClass(), ' ', $this->getElement(3) ?>" id="<?php echo $this->getHTMLId() ?>">
         <?php if ($this->getLabel()): ?>
             <legend id="<?php echo $this->getFieldId() ?>"><?php echo $this->getLabel() ?></legend>
         <?php endif ?>
